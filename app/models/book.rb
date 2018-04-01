124636a08e358dd4b08ec87b89dbf0ad80e00cea
kylesallette <kylesallette@gmail.com>
2018-04-01 15:58:32 -0600

    5th test

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,22 @@
 class Book < ApplicationRecord
    has_many :reviews
 
+
+
+  def average_rating
+    reviews.average(:rating)
+  end
+
+
+  def highest_rating
+    reviews.maximum(:rating)
+  end
+
+  def lowest_rating
+    reviews.minimum(:rating)
+  end
+
+
+
+
 end
--- spec/user_sees_book_reviews_spec.rb
+++ spec/user_sees_book_reviews_spec.rb
@@ -30,3 +30,66 @@ require 'rails_helper'
       end
     end
   end
+
+
+  describe "user visits book show and sees average of reviews"  do
+    context "visit [book_path]"  do
+      it "they see book average review"  do
+        book = Book.create(id: 1, title: "moby dick")
+        user1 = User.create(name:"kyle")
+        user2 = User.create(name:"bob")
+        review = book.reviews.create(body: "hello", rating: 1, user_id: user1.id)
+        review = book.reviews.create(body: "hello", rating: 3, user_id: user2.id)
+
+        visit "books/1"
+
+        expect(page).to have_content(book.title)
+        expect(page).to have_content("hello")
+        expect(page).to have_content("2")
+        expect(page).to have_content(user1.name)
+        expect(page).to have_content(user2.name)
+      end
+    end
+  end
+
+
+    describe "user visits book show and sees lowest of reviews"  do
+      context "visit [book_path]"  do
+        it "they see book lowest review"  do
+          book = Book.create(id: 1, title: "moby dick")
+          user1 = User.create(name:"kyle")
+          user2 = User.create(name:"bob")
+          review = book.reviews.create(body: "hello", rating: 1, user_id: user1.id)
+          review = book.reviews.create(body: "hello", rating: 3, user_id: user2.id)
+
+          visit "books/1"
+
+          expect(page).to have_content(book.title)
+          expect(page).to have_content("hello")
+          expect(page).to have_content("1")
+          expect(page).to have_content(user1.name)
+          expect(page).to have_content(user2.name)
+        end
+      end
+    end
+
+    describe "user visits book show and sees highest of reviews"  do
+      context "visit [book_path]"  do
+        it "they see book highest review"  do
+          book = Book.create(id: 1, title: "moby dick")
+          user1 = User.create(name:"kyle")
+          user2 = User.create(name:"bob")
+          review = book.reviews.create(body: "hello", rating: 1, user_id: user1.id)
+          review = book.reviews.create(body: "hello", rating: 3, user_id: user2.id)
+          review = book.reviews.create(body: "hello", rating: 8, user_id: user2.id)
+
+          visit "books/1"
+
+          expect(page).to have_content(book.title)
+          expect(page).to have_content("hello")
+          expect(page).to have_content(8)
+          expect(page).to have_content(user1.name)
+          expect(page).to have_content(user2.name)
+        end
+      end
+    end
